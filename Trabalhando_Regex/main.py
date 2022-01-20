65742d32a98aac223c4beee2ca511b1b49043a5e
Gabriel Silvério <gabriel.v.silverio@gmail.com>
2022-01-20 13:22:19 -0300

    Create main.py

---
+++ Trabalhando_Regex/main.py
@@ -0,0 +1,154 @@
+
+
+'''crie uma regex compilada capaz de extrair as datas citadas no texto da decisão.(ignore a data na assinatura)'''
+
+acordao = """
+Trata-se de agravo interposto contra decisão denegatória de recurso 
+especial interposto pela BRASIL TELECOM S/A, com fundamento no art. 105, inc. 
+III, alíneas a, da Constituição Federal, em que alegou violação ao disposto nos arts. 
+535, II, e 538, parágrafo único, do Código de Processo Civil, apontando omissão no v. 
+acórdão recorrido que teria se escusado de analisar alegada ofensa ao princípio 
+constitucional da isonomia, previsto no art. 5º, caput e inciso I, da Constituição 
+Federal, e se insurgindo contra a aplicação de multa aos embargos de declaração 
+opostos na origem.
+
+É o relatório.
+Decido.
+
+Inúmeras são as decisões proferidas neste c. Superior Tribunal de 
+Justiça afastando a existência de omissão do v. acórdão proferido na instância a quo, 
+que rejeitou a alegação de prescrição formulada pela BRASIL TELECOM S/A, com 
+base na jurisprudência pacificada pela eg. Segunda Seção desta c. Corte Superior, 
+nos moldes do art. 543-C do CPC, no sentido de que: "nas demandas em que se 
+discute o direito à complementação de ações em face do descumprimento de contrato 
+de participação financeira firmado com sociedade anônima, a pretensão é de 
+natureza pessoal e prescreve nos prazos previstos no artigo 177 do Código Civil 
+revogado e artigos 205 e 2.028 do Novo Código Civil" (REsp 1.033.241/RS, Rel. Min. 
+Aldir Passarinho Junior, DJe de 5/11/2008).
+
+Documento: 27772548 - Despacho / Decisão - Site certificado - DJe: 04/04/2013 Página 1 de 3
+
+Com efeito, é pacífico o entendimento nos casos idênticos ao dos autos, 
+de que "se os fundamentos adotados bastam para justificar o concluído na decisão, o 
+julgador não está obrigado a rebater, um a um, os argumentos utilizados pela parte" 
+(AREsp 235.823/SC, Rel. Min. Sidnei Beneti, DJe de 3/10/2012). Confira-se os 
+seguintes julgados: AREsp 196.150/SC, Rel. Min. Nancy Andrighi, DJe de 
+14/9/2012; AREsp 224.239/SC, Rel. Min. Massami Uyeda, DJe de 4/10/2012; AREsp 
+230.079, Rel. Min. Antonio Carlos Ferreira, DJe de 3/10/2012; AG 1.431.263/SC, 
+Rel. Min. Ricardo Villas Bôas Cueva, DJe de 20/9/2012; AREsp 236.316/SC, Rel. 
+Min. Marco Buzzi, DJe de 3/10/2012, e; Ag 1.426.474/SC, Rel. Min. Maria Isabel 
+Gallotti, DJe de 16/11/2011). 
+
+A alegada omissão também é rechaçada porque a recorrente pretende 
+seja examinada matéria de índole constitucional, que foge da competência deste c. 
+Superior Tribunal de Justiça. Nesse sentido, são exemplo, entre várias outras, as 
+decisões a seguir: AREsp 225.309/SC, Rel. Min. Luis Felipe Salomão, DJe de 
+28/9/2012 e AREsp 219.410/SC, Rel. Min. Raul Araújo, DJe de 2/10/2012; AREsp 
+235.789/SC, Rel. Min. Sidinei Beneti, DJe de 3/10/2012, e; AREsp 195.560/SC, Rel. 
+Min. Nancy Andrighi, DJe de 19/9/2012.
+
+No que diz respeito à multa do art. 538, parágrafo único, do Código de 
+Processo Civil, constata-se que a recorrente procurou, com os embargos de declaração, 
+satisfazer os pressupostos de admissibilidade dos recursos para os Tribunais 
+Superiores, mais especificamente o prequestionamento, o que afasta o seu caráter 
+protelatório. Incide, assim, in casu, a Súmula nº 98 do STJ, que dispõe: "Embargos de 
+declaração manifestados com notório propósito de prequestionamento não tem 
+caráter protelatório".
+
+Nesse sentido é a jurisprudência desta eg. Corte Superior, in verbis:
+
+"AGRAVO REGIMENTAL - AÇÃO DE 
+COMPLEMENTAÇÃO DE AÇÕES - BRASIL TELECOM S/A - 
+CONTRATO DE PARTICIPAÇÃO FINANCEIRA - ART. 538, 
+PARÁGRAFO ÚNICO, DO CPC - MULTA - AFASTAMENTO - 
+NECESSIDADE - INCIDÊNCIA DA SÚMULA N. 98/STJ - VALOR 
+PATRIMONIAL DA AÇÃO - DATA DA INTEGRALIZAÇÃO DO 
+CAPITAL - BALANCETE MENSAL CORRESPONDENTE - VERBA 
+HONORÁRIA - AUSÊNCIA DE CONDENAÇÃO EM DINHEIRO - 
+FIXAÇÃO COM BASE NOS ARTS. 20, § 4º, DO CPC - AGRAVO 
+REGIMENTAL PARCIALMENTE PROVIDO.
+
+Documento: 27772548 - Despacho / Decisão - Site certificado - DJe: 04/04/2013 Página 2 de 3
+
+1. A multa imposta com fundamento no artigo 538, 
+parágrafo único, do CPC, é de ser afastada, quando, embora tenham 
+sido rejeitados os embargos de declaração, estes tenham sido opostos 
+com a finalidade de prequestionamento da questão federal, conforme 
+disposto na Súmula nº. 98 desta Corte, in verbis: 'Embargos de 
+declaração manifestados com notório propósito de prequestionamento 
+não tem caráter protelatório'.
+
+(...).
+4. Agravo regimental parcialmente provido"
+(AgRg nos EDcl no Ag n 928.938/RS, 3ª Turma, Rel. 
+Min. Massami Uyeda, DJe de 5/11/2009)
+
+E ainda: REsp n. 954.286/RS, Rel. Min. Maria Isabel Gallotti, DJe de 
+16/8/2011; AREsp n. 195.560/SC, Rel. Min. Nancy Andrighi, DJe de 19/9/2012; 
+AREsp n. 235.913/SC, Rel. Min. Antonio Carlos Ferreira, DJe de 2/10/2012; AREsp 
+n. 224.239/SC, Rel. Min. Massami Uyeda, Dje de 4/12/2012, e; AgRg no Ag n. 
+1.399.398/SC, Rel. Min. Nancy Andrighi, DJe de 15/8/2011.
+
+Por todo o exposto, com fulcro no art. 544, § 4º, inciso II, alínea c, do 
+Código de Processo Civil, conheço do agravo para dar parcial provimento ao 
+recurso especial, determinando o afastamento da multa arbitrada nos embargos de 
+declaração pelo eg. Tribunal a quo.
+
+P. e I.
+Brasília (DF), 22 de março de 2013.
+
+MINISTRO FELIX FISCHER
+
+Presidente
+
+Documento: 27772548 - Despacho / Decisão - Site certificado - DJe: 04/04/2013 Página 3 de 3
+"""
+
+import re
+
+'''cpag1 = []
+cpag2 = []
+cpag3 = []
+acordao = acordao.strip()
+acordao = acordao.split(f'Documento: ')
+for i, v in enumerate(acordao):
+    if i != 0:
+        if i == 1:
+            cpag1.append(v.count('AResp'))
+            cpag1.append(v.count('REsp'))
+            cpag1.append(v.count('Ag'))
+        elif i == 2:
+            cpag2.append(v.count('AResp'))
+            cpag2.append(v.count('REsp'))
+            cpag2.append(v.count('Ag'))
+        elif i == 3: 
+            cpag3.append(v.count('AResp'))
+            cpag3.append(v.count('REsp'))
+            cpag3.append(v.count('Ag'))
+print(cpag1)
+print(f'Existe {cpag1[0]} AResp, {cpag1[1]} REsp, {cpag1[2]} Ag no 1º página')
+print(f'Existe {cpag2[0]} AResp, {cpag2[1]} REsp, {cpag2[2]} Ag no 2º página')
+print(f'Existe {cpag3[0]} AResp, {cpag3[1]} REsp, {cpag3[2]} Ag no 3º página')
+print(f'Em todo acordao, existe {cpag1[0]+cpag2[0]+cpag3[0]} AResp, {cpag1[1]+cpag2[1]+cpag3[1]} REsp, {cpag1[2]+cpag2[2]+cpag3[2]} Ag')'''
+
+'''from datetime import datetime
+import re
+
+s= re.compile(r"\d{1,2}/\d{1,2}/\d{4}")
+datastexto= s.findall(acordao)
+datas = []
+for i, v in enumerate(datastexto):
+    datas.append(datetime.strptime(v, '%d/%m/%Y').date())
+data_acordao = datas[0]
+for i, v in enumerate(datas):
+    if i != 0:
+        print(f'{v} - {data_acordao} = {v - data_acordao}')'''
+
+from datetime import datetime
+import re
+
+s= re.compile(r"\d{1,2}/\d{1,2}/\d{4}")
+datastexto= s.findall(acordao)
+datas = []
+for i, v in enumerate(datastexto):
+    datas.append(datetime.strptime(v, '%d/%m/%Y').date())
